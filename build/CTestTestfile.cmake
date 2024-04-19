2c7d44c64bc383eef1a53e635b3488120e3429d0
alpertunga-bile <kingdom19online@hotmail.com>
2024-04-19 11:53:52 +0300

    build directory is deleted

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/alper.bile/Projects/pxd-stl
-# Build directory: C:/Users/alper.bile/Projects/pxd-stl/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
