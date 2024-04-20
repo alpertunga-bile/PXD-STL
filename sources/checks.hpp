78740c96e7b642c5e8439d0af19e7de4f2bf0990
alpertunga-bile <kingdom19online@hotmail.com>
2024-04-20 17:02:19 +0300

    nothing is changed

--- sources/checks.hpp
+++ sources/checks.hpp
@@ -14,8 +14,12 @@
 #define PXD_ASSERT(exp) assert_func(exp, __FILE__, __LINE__, __FUNCTION_NAME__)
 #define PXD_TODO(msg) todo_func(msg, __FILE__, __LINE__, __FUNCTION_NAME__)
 
-inline void assert_func(bool expression, const char *filename, int line,
-                        const char *function_name) {
+inline void
+assert_func(bool expression,
+            const char* filename,
+            int line,
+            const char* function_name)
+{
   if (expression) {
     return;
   }
@@ -23,16 +27,25 @@ inline void assert_func(bool expression, const char *filename, int line,
   printf("Assertion is FAILED at:\n    Filename : %s\n    Line     : %d\n    "
          "Function : "
          "[%s]\n",
-         filename, line, function_name);
+         filename,
+         line,
+         function_name);
   exit(EXIT_FAILURE);
 }
 
-inline void todo_func(const char *message, const char *filename, int line,
-                      const char *function_name) {
+inline void
+todo_func(const char* message,
+          const char* filename,
+          int line,
+          const char* function_name)
+{
   printf("TODO message:\n    Message : %s\n    Filename : %s\n    Line     : "
          "%d\n    "
          "Function : "
          "[%s]\n",
-         message, filename, line, function_name);
+         message,
+         filename,
+         line,
+         function_name);
   exit(EXIT_FAILURE);
 }
