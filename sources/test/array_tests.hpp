a690e48520f0db868706ffe02d0ef80284f53cad
alpertunga-bile <kingdom19online@hotmail.com>
2024-04-22 10:34:54 +0300

    temp is created with new

--- sources/test/array_tests.hpp
+++ sources/test/array_tests.hpp
@@ -23,12 +23,10 @@ print_array(const char* name, pxd::Array<int>& arr)
 */
 
 namespace pxd {
-class ArrayTests : public ITest
-{
+class ArrayTests : public ITest {
 public:
-  void start_test() override
-  {
-    int temp[N];
+  void start_test() override {
+    int *temp = new int[N];
 
     for (int i = 0; i < N; i++) {
       temp[i] = i;
@@ -40,12 +38,13 @@ public:
     start_expand_test<int>(temp);
     start_index_test<int>(temp);
     start_resize_test<int>(temp);
+
+    delete[] temp;
   }
 
 private:
-  template<typename T>
-  bool check_arrays(T* first_array, T* second_array, int size)
-  {
+  template <typename T>
+  bool check_arrays(T *first_array, T *second_array, int size) {
     for (int i = 0; i < size; i++) {
       if (first_array[i] != second_array[i]) {
         return false;
@@ -55,13 +54,9 @@ private:
     return true;
   }
 
-  template<typename T>
-  bool check_arrays(T* first_array,
-                    int first_start,
-                    T* second_array,
-                    int second_start,
-                    int size)
-  {
+  template <typename T>
+  bool check_arrays(T *first_array, int first_start, T *second_array,
+                    int second_start, int size) {
     int second_index = second_start;
     int end_index = first_start + size;
 
@@ -77,39 +72,31 @@ private:
     return true;
   }
 
-  template<typename T>
-  void start_init_with_array_test(T* temp)
-  {
+  template <typename T> void start_init_with_array_test(T *temp) {
     Array<T> arr(temp, N);
 
     test_results["init with array test"] =
-      check_arrays<T>(temp, arr.get_ptr(), N);
+        check_arrays<T>(temp, arr.get_ptr(), N);
   }
 
-  template<typename T>
-  void start_copy_to_test(T* temp)
-  {
+  template <typename T> void start_copy_to_test(T *temp) {
     Array<T> arr(temp, N);
 
     Array<T> t(N);
     arr.copy_to(t);
 
     test_results["copy_to test"] =
-      check_arrays<T>(t.get_ptr(), arr.get_ptr(), N);
+        check_arrays<T>(t.get_ptr(), arr.get_ptr(), N);
   }
 
-  template<typename T>
-  void start_bool_compare_test()
-  {
+  template <typename T> void start_bool_compare_test() {
     Array<T> arr(10);
     Array<T> temp(10);
 
     test_results["bool compare test"] = arr != temp && arr == arr;
   }
 
-  template<typename T>
-  void start_expand_test(T* temp)
-  {
+  template <typename T> void start_expand_test(T *temp) {
     Array<T> arr(temp, N);
     Array<T> t(10);
 
@@ -118,31 +105,27 @@ private:
     t.expand(arr);
 
     test_results["expand test"] =
-      check_arrays(t.get_ptr(), arr.get_ptr(), N) &&
-      check_arrays(t.get_ptr(), N, arr.get_ptr(), 0, N);
+        check_arrays(t.get_ptr(), arr.get_ptr(), N) &&
+        check_arrays(t.get_ptr(), N, arr.get_ptr(), 0, N);
   }
 
-  template<typename T>
-  void start_index_test(T* temp)
-  {
+  template <typename T> void start_index_test(T *temp) {
     Array<T> arr(temp, N);
 
     test_results["index test"] = arr[-1] == arr[N - 1] && arr[2] == 2;
   }
 
-  template<typename T>
-  void start_resize_test(T* temp)
-  {
+  template <typename T> void start_resize_test(T *temp) {
     int new_size = 5;
     Array<T> arr(temp, N);
 
     arr.resize(new_size);
 
     test_results["resize test"] =
-      check_arrays<T>(temp, arr.get_ptr(), new_size);
+        check_arrays<T>(temp, arr.get_ptr(), new_size);
   }
 
 private:
   int N = 10;
 };
-}
+} // namespace pxd
