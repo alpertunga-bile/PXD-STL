6d8fc6fc2044e3f4f109c8d23093427df93f7d64
alpertunga-bile <kingdom19online@hotmail.com>
2024-04-22 12:00:09 +0300

    new test is added

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
-#include "sources/test/test_manager.hpp"
+#define ENABLE_ASSERTS
 
 #include "sources/test/array_tests.hpp"
+#include "sources/test/linked_list_tests.hpp"
+#include "sources/test/test_manager.hpp"
 
-int
-main()
-{
+
+int main() {
   pxd::TestManager test_manager;
 
   pxd::ArrayTests array_tests;
+  pxd::LinkedListTests linked_list_tests;
 
   test_manager.add_test("Array Tests", array_tests);
+  test_manager.add_test("Linked Link Tests", linked_list_tests);
 
   test_manager.print_results();
 
