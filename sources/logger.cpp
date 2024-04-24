70dfc3bba16c2aae8334b9c28540b0efcc417817
alpertunga-bile <kingdom19online@hotmail.com>
2024-04-24 11:35:18 +0300

    logger is added

---
+++ sources/logger.cpp
@@ -0,0 +1,62 @@
+#include "logger.hpp"
+
+#include "string.hpp"
+
+#include <filesystem>
+#include <format>
+#include <fstream>
+
+void create_log_file() {
+  if (std::filesystem::exists(std::filesystem::path("app.log"))) {
+    std::filesystem::remove(std::filesystem::path("app.log"));
+  } else {
+    std::ofstream log_file("app.log");
+    log_file.close();
+  }
+}
+
+void write_to_log_file(const char *log_string) {
+  std::ofstream log_file("app.log", std::ios_base::app);
+  log_file << log_string;
+  log_file.close();
+}
+
+String get_output_string(const char *filename, int line,
+                         const char *function_name) {
+  auto base_filename = std::filesystem::path(filename).filename().string();
+
+  auto formattedString =
+      std::format("{} /_\\ {} /_\\ {}", base_filename, line, function_name);
+
+  return String(formattedString);
+}
+
+void log_info(const char *msg, const char *time, const char *filename, int line,
+              const char *function_name) {
+  std::string formatStr =
+      std::format("[{}] /_\\ [    INFO] /_\\ {} /_\\ {}\n", time, msg,
+                  get_output_string(filename, line, function_name).c_str());
+
+  printf("%s", formatStr.c_str());
+  write_to_log_file(formatStr.c_str());
+}
+
+void log_warning(const char *msg, const char *time, const char *filename,
+                 int line, const char *function_name) {
+  std::string formatStr =
+      std::format("[{}] /_\\ [ WARNING] /_\\ {} /_\\ {}\n", time, msg,
+                  get_output_string(filename, line, function_name).c_str());
+
+  printf("%s", formatStr.c_str());
+  write_to_log_file(formatStr.c_str());
+}
+
+void log_error(const char *msg, const char *time, const char *filename,
+               int line, const char *function_name) {
+  std::string formatStr =
+      std::format("[{}] /_\\ [  FAILED] /_\\ {} /_\\ {}\n", time, msg,
+                  get_output_string(filename, line, function_name).c_str());
+
+  printf("%s", formatStr.c_str());
+  write_to_log_file(formatStr.c_str());
+}
---
+++ sources/logger.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "checks.hpp"
+
+void create_log_file();
+
+void log_info(const char *msg, const char *time, const char *filename, int line,
+              const char *function_name);
+
+void log_warning(const char *msg, const char *time, const char *filename,
+                 int line, const char *function_name);
+
+void log_error(const char *msg, const char *time, const char *filename,
+               int line, const char *function_name);
+
+#if defined(_DEBUG)
+#define LOG_INFO(msg)                                                          \
+  log_info(msg, __TIME__, __FILE__, __LINE__, __FUNCTION_NAME__)
+#define LOG_WARNING(msg)                                                       \
+  log_warning(msg, __TIME__, __FILE__, __LINE__, __FUNCTION_NAME__)
+#define LOG_ERROR(msg)                                                         \
+  log_error(msg, __TIME__, __FILE__, __LINE__, __FUNCTION_NAME__)
+#else
+#define LOG_INFO(msg)
+#define LOG_WARNING(msg)
+#define LOG_ERROR(msg)
+#endif
