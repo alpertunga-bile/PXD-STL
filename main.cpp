53362c2de917b314b332ebc1bec815e40ed66d09
alpertunga-bile <kingdom19online@hotmail.com>
2024-04-29 16:56:22 +0300

    dynamic array tests are added

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include "test/array_tests.hpp"
 #include "test/binary_search_tree_tests.hpp"
+#include "test/dynamic_array_tests.hpp"
 #include "test/linked_list_tests.hpp"
 #include "test/queue_tests.hpp"
 #include "test/stack_tests.hpp"
@@ -16,12 +17,14 @@ int main() {
   pxd::BinarySearchTreeTests binary_search_tree_tests;
   pxd::StackTests stack_tests;
   pxd::QueueTests queue_tests;
+  pxd::DynamicArrayTests dynamic_array_tests;
 
   test_manager.add_test("Array Tests", array_tests);
   test_manager.add_test("Linked Link Tests", linked_list_tests);
   test_manager.add_test("Binary Search Tree Tests", binary_search_tree_tests);
   test_manager.add_test("Stack Tests", stack_tests);
   test_manager.add_test("Queue Tests", queue_tests);
+  test_manager.add_test("Dynamic Array Tests", dynamic_array_tests);
 
   test_manager.print_results();
 
---
+++ test/dynamic_array_tests.hpp
@@ -0,0 +1,115 @@
+#pragma once
+
+#include "dynamic_array.hpp"
+#include "test_utils.hpp"
+
+namespace pxd {
+class DynamicArrayTests : public ITest {
+public:
+  void start_test() override {
+    int *temp_arr = new int[N];
+
+    for (int i = 0; i < N; i++) {
+      temp_arr[i] = i + 1;
+    }
+
+    start_copy_ctor_test(temp_arr);
+    start_move_ctor_test(temp_arr);
+    start_assign_ctor_test(temp_arr);
+    start_expand_test(temp_arr);
+    start_add_test(temp_arr);
+    start_shrink_test(temp_arr);
+    start_index_test(temp_arr);
+    start_resize_test(temp_arr);
+
+    delete[] temp_arr;
+  }
+
+  void start_copy_ctor_test(int *temp_arr) {
+    DynamicArray<int> darray;
+    darray.expand(temp_arr, N);
+
+    DynamicArray<int> dtemp(darray);
+
+    test_results["copy ctor"] =
+        check_arrays<int>(darray.get_data(), dtemp.get_data(), N);
+  }
+
+  void start_move_ctor_test(int *temp_arr) {
+    DynamicArray<int> darray;
+    darray.expand(temp_arr, N);
+
+    DynamicArray<int> dtemp(std::move(darray));
+
+    test_results["move ctor"] =
+        check_arrays<int>(darray.get_data(), dtemp.get_data(), N);
+  }
+
+  void start_assign_ctor_test(int *temp_arr) {
+    DynamicArray<int> darray;
+    darray.expand(temp_arr, N);
+
+    DynamicArray<int> dtemp = darray;
+
+    test_results["assign ctor"] =
+        check_arrays<int>(darray.get_data(), dtemp.get_data(), N);
+  }
+
+  void start_expand_test(int *temp_arr) {
+    DynamicArray<int> darray;
+    darray.expand(temp_arr, N);
+
+    test_results["expand from 0"] =
+        check_arrays<int>(darray.get_data(), temp_arr, N);
+
+    darray.expand(temp_arr, N);
+
+    test_results["expand with exist"] =
+        check_arrays<int>(darray.get_data(), N, temp_arr, 0, N);
+  }
+
+  void start_add_test(int *temp_arr) {
+    DynamicArray<int> darray;
+
+    for (int i = 0; i < N; i++) {
+      darray.add(temp_arr[i]);
+    }
+
+    test_results["add"] = check_arrays<int>(darray.get_data(), temp_arr, N);
+  }
+
+  void start_shrink_test(int *temp_arr) {
+    DynamicArray<int> darray;
+
+    for (int i = 0; i < 7; i++) {
+      darray.add(temp_arr[i]);
+    }
+
+    darray.shrink();
+
+    test_results["shrink"] = check_arrays<int>(darray.get_data(), temp_arr,
+                                               darray.get_element_count());
+  }
+
+  void start_index_test(int *temp_arr) {
+    DynamicArray<int> darray;
+    darray.expand(temp_arr, N);
+
+    test_results["index"] = darray[5] == temp_arr[5];
+    test_results["negative index"] = darray[-1] == temp_arr[N - 1];
+  }
+
+  void start_resize_test(int *temp_arr) {
+    DynamicArray<int> darray(20);
+
+    for (int i = 0; i < N; i++) {
+      darray[i] = temp_arr[i];
+    }
+
+    test_results["resize"] = check_arrays<int>(darray.get_data(), temp_arr, N);
+  }
+
+private:
+  int N = 10;
+};
+} // namespace pxd
