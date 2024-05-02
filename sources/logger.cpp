5a49f376d755ca15ddb3493ccf77ed7a812f1878
alpertunga-bile <kingdom19online@hotmail.com>
2024-05-02 17:13:10 +0300

    logger class is added

--- sources/logger.cpp
+++ sources/logger.cpp
@@ -2,25 +2,9 @@
 
 #include "string.hpp"
 
-#include <filesystem>
 #include <format>
-#include <fstream>
 
-void create_log_file() {
-  if (std::filesystem::exists(std::filesystem::path("app.log"))) {
-    std::filesystem::remove(std::filesystem::path("app.log"));
-  } else {
-    std::ofstream log_file("app.log");
-    log_file.close();
-  }
-}
-
-void write_to_log_file(const char *log_string) {
-  std::ofstream log_file("app.log", std::ios_base::app);
-  log_file << log_string;
-  log_file.close();
-}
-
+namespace pxd {
 String get_output_string(const char *filename, int line,
                          const char *function_name) {
   auto base_filename = std::filesystem::path(filename).filename().string();
@@ -31,8 +15,24 @@ String get_output_string(const char *filename, int line,
   return String(formattedString);
 }
 
-void log_info(const char *msg, const char *time, const char *filename, int line,
-              const char *function_name) {
+constexpr Logger::Logger() {
+  log_file.open("app.log");
+  log_file.close();
+
+  log_file.open("app.log", std::ios_base::app);
+}
+
+inline Logger::~Logger() noexcept {
+  if (log_file.is_open()) {
+    log_file.close();
+  }
+
+  delete instance;
+}
+
+constexpr void Logger::log_info(const char *msg, const char *time,
+                                const char *filename, int line,
+                                const char *function_name) noexcept {
   std::string formatStr =
       std::format("[{}] /_\\ [    INFO] /_\\ {} /_\\ {}\n", time, msg,
                   get_output_string(filename, line, function_name).c_str());
@@ -41,8 +41,9 @@ void log_info(const char *msg, const char *time, const char *filename, int line,
   write_to_log_file(formatStr.c_str());
 }
 
-void log_warning(const char *msg, const char *time, const char *filename,
-                 int line, const char *function_name) {
+constexpr void Logger::log_warning(const char *msg, const char *time,
+                                   const char *filename, int line,
+                                   const char *function_name) noexcept {
   std::string formatStr =
       std::format("[{}] /_\\ [ WARNING] /_\\ {} /_\\ {}\n", time, msg,
                   get_output_string(filename, line, function_name).c_str());
@@ -51,8 +52,9 @@ void log_warning(const char *msg, const char *time, const char *filename,
   write_to_log_file(formatStr.c_str());
 }
 
-void log_error(const char *msg, const char *time, const char *filename,
-               int line, const char *function_name) {
+constexpr void Logger::log_error(const char *msg, const char *time,
+                                 const char *filename, int line,
+                                 const char *function_name) noexcept {
   std::string formatStr =
       std::format("[{}] /_\\ [  FAILED] /_\\ {} /_\\ {}\n", time, msg,
                   get_output_string(filename, line, function_name).c_str());
@@ -60,3 +62,17 @@ void log_error(const char *msg, const char *time, const char *filename,
   printf("%s", formatStr.c_str());
   write_to_log_file(formatStr.c_str());
 }
+
+Logger *Logger::get_instance() {
+  if (instance == nullptr) {
+    instance = new Logger();
+  }
+
+  return instance;
+}
+
+void Logger::write_to_log_file(const char *log_string) noexcept {
+  log_file << log_string;
+}
+
+} // namespace pxd
--- sources/logger.hpp
+++ sources/logger.hpp
@@ -1,25 +1,48 @@
 #pragma once
 
 #include "checks.hpp"
+#include <filesystem>
 
-void create_log_file();
+namespace pxd {
+class Logger {
+public:
+  constexpr Logger();
+  Logger(const Logger &other) = delete;
+  Logger &operator=(const Logger &other) = delete;
+  Logger(Logger &&other) = delete;
+  Logger &operator=(Logger &&other) = delete;
+  inline ~Logger() noexcept;
 
-void log_info(const char *msg, const char *time, const char *filename, int line,
-              const char *function_name);
+  constexpr void log_info(const char *msg, const char *time,
+                          const char *filename, int line,
+                          const char *function_name) noexcept;
+  constexpr void log_warning(const char *msg, const char *time,
+                             const char *filename, int line,
+                             const char *function_name) noexcept;
+  constexpr void log_error(const char *msg, const char *time,
+                           const char *filename, int line,
+                           const char *function_name) noexcept;
 
-void log_warning(const char *msg, const char *time, const char *filename,
-                 int line, const char *function_name);
+  static Logger *get_instance() noexcept;
 
-void log_error(const char *msg, const char *time, const char *filename,
-               int line, const char *function_name);
+private:
+  void write_to_log_file(const char *log_string) noexcept;
+
+private:
+  std::ofstream log_file;
+  static Logger *instance;
+};
+
+Logger *logger = Logger::get_instance();
+} // namespace pxd
 
 #if defined(_DEBUG)
 #define LOG_INFO(msg)                                                          \
-  log_info(msg, __TIME__, __FILE__, __LINE__, __FUNCTION_NAME__)
+  logger->log_info(msg, __TIME__, __FILE__, __LINE__, __FUNCTION_NAME__)
 #define LOG_WARNING(msg)                                                       \
-  log_warning(msg, __TIME__, __FILE__, __LINE__, __FUNCTION_NAME__)
+  logger->log_warning(msg, __TIME__, __FILE__, __LINE__, __FUNCTION_NAME__)
 #define LOG_ERROR(msg)                                                         \
-  log_error(msg, __TIME__, __FILE__, __LINE__, __FUNCTION_NAME__)
+  logger->log_error(msg, __TIME__, __FILE__, __LINE__, __FUNCTION_NAME__)
 #else
 #define LOG_INFO(msg)
 #define LOG_WARNING(msg)
