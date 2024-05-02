d1e52c2918192ec5f579ae0fa97387ec138b5d20
alpertunga-bile <kingdom19online@hotmail.com>
2024-05-02 16:22:09 +0300

    additional infos are added to test outputs

--- test/test_manager.hpp
+++ test/test_manager.hpp
@@ -13,7 +13,19 @@ public:
     tests[name] = test.test_results;
   }
 
+  void reset_test_counts() {
+    total_tests = 0;
+    passed_tests = 0;
+  }
+
+  void check_test_counts(bool result) {
+    total_tests++;
+    passed_tests = result ? passed_tests + 1 : 0;
+  }
+
   void print_results() {
+    reset_test_counts();
+
     for (auto &[test_name, test] : tests) {
       printf(
           "---------------------------------------------------------------\n");
@@ -22,14 +34,24 @@ public:
       for (auto &[test_case_name, test_result] : test) {
         printf("  %-30s -> %-6s\n", test_case_name.c_str(),
                test_result == true ? "Passed" : "Failed");
+
+        check_test_counts(test_result);
       }
     }
+
+    printf("---------------------------------------------------------------\n");
+
+    float passed_ratio = static_cast<float>(passed_tests) /
+                         static_cast<float>(total_tests) * 100.0f;
+    printf(" Passed Tests : %d | Total Tests : %d | Passed Ratio : %.2f\n",
+           passed_tests, total_tests, passed_ratio);
 
     printf("---------------------------------------------------------------\n");
   }
 
   void save_results(const char *filename = "test_results.txt") {
     std::ofstream test_file(filename, std::ios::out);
+    reset_test_counts();
 
     for (auto &[test_name, test] : tests) {
       test_file << "-----------------------------------------------------------"
@@ -41,8 +63,20 @@ public:
         test_file << "   " << std::left << std::setw(30) << test_case_name
                   << " -> " << std::left << std::setw(6)
                   << (test_result == true ? "Passed" : "Failed") << "\n";
+
+        check_test_counts(test_result);
       }
     }
+
+    test_file << "-----------------------------------------------------------"
+                 "----\n";
+
+    float passed_ratio = static_cast<float>(passed_tests) /
+                         static_cast<float>(total_tests) * 100.f;
+
+    test_file << " Passed Tests : " << passed_tests
+              << " | Total Tests : " << total_tests
+              << " | Passed Ratio : " << passed_ratio << "\n";
 
     test_file << "-----------------------------------------------------------"
                  "----\n";
@@ -52,5 +86,7 @@ public:
 
 private:
   std::unordered_map<std::string, std::unordered_map<std::string, bool>> tests;
+  int passed_tests = 0;
+  int total_tests = 0;
 };
 } // namespace pxd
