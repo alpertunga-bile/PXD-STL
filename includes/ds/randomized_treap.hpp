a409ef40581b632935bc20ea00971688f1f8e268
alpertunga-bile <kingdom19online@hotmail.com>
2024-07-26 15:23:20 +0300

    randomized_treap is added

--- includes/ds/randomized_treap.hpp
+++ includes/ds/randomized_treap.hpp
@@ -6,7 +6,25 @@
 #include "random_gen.hpp"
 
 namespace pxd {
+/// @brief Randomized treap to use as (most possibly) balanced BST
+/// @tparam T value type
 template <typename T> class RandomizedTreap {
+public:
+  RandomizedTreap() = default;
+  RandomizedTreap(const RandomizedTreap<T> &other) = default;
+  RandomizedTreap &operator=(const RandomizedTreap<T> &other) = default;
+  RandomizedTreap(RandomizedTreap &&other) = default;
+  RandomizedTreap &operator=(RandomizedTreap &&other) = default;
+  ~RandomizedTreap() = default;
+
+  void insert(T &key) { treap.insert(key, get_random_value(0.0, 1.0)); }
+
+  void constains(const T &key) { return treap.contains(key); }
+
+  void remove(const T &key) { return treap.remove(key); }
+
+  T min() { return treap.min(); }
+  T max() { return treap.max(); }
 
 private:
   Treap<T> treap;
