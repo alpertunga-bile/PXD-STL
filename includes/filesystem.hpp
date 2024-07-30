b66ce307d2ef8b9aaac6e548efe4a767efe1b147
alpertunga-bile <kingdom19online@hotmail.com>
2024-07-30 12:02:22 +0300

    filesystem is updated

--- includes/filesystem.hpp
+++ includes/filesystem.hpp
@@ -8,14 +8,14 @@ inline bool exists(const char *path);
 inline bool is_dir(const char *path);
 inline bool is_file(const char *path);
 
-template <typename... P> inline String join(const P &...paths) {
-  String values[] = {paths...};
+template <typename... P> inline String join_paths(const P &...paths) {
+  const String values[] = {paths...};
   const int n = sizeof...(paths) - 1;
 
 #if defined(__WIN32__) || defined(_WIN32)
-  String path_sep = "\\";
+  const String path_sep = "\\";
 #else
-  String path_sep = "/";
+  const String path_sep = "/";
 #endif
 
   String path = "";
