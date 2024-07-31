0b100795d7a133c81538c97e30088ad44111de9c
alpertunga-bile <kingdom19online@hotmail.com>
2024-07-31 15:32:06 +0300

    fixed with clang-tidy

--- includes/checks.hpp
+++ includes/checks.hpp
@@ -1,20 +1,22 @@
 #pragma once
 
-#ifndef __FUNCTION_NAME__
+#ifndef PXD_CHECKS_FUNCTION_NAME
 #ifdef _WIN32
-#define __FUNCTION_NAME__ __FUNCTION__
+#define PXD_CHECKS_FUNCTION_NAME __FUNCTION__
 #else
-#define __FUNCTION_NAME__ __func__
+#define PXD_CHECKS_FUNCTION_NAME __func__
 #endif
 #endif
 
-constexpr auto IS_VALID(void *obj) -> bool { return obj != nullptr; }
+constexpr auto IS_VALID_PTR(void *obj) -> bool { return obj != nullptr; }
 
-#ifdef PXD_ENABLE_ASSERTS || defined(_DEBUG)
-#define PXD_ASSERT(exp) assert_func(exp, __FILE__, __LINE__, __FUNCTION_NAME__)
+#if defined(PXD_ENABLE_ASSERTS) || defined(_DEBUG)
+#define PXD_ASSERT(exp)                                                        \
+  assert_func(exp, __FILE__, __LINE__, PXD_CHECKS_FUNCTION_NAME)
 #define PXD_ASSERT_MSG(exp, msg)                                               \
-  assert_msg_func(msg, exp, __FILE__, __LINE__, __FUNCTION_NAME__)
-#define PXD_TODO(msg) todo_func(msg, __FILE__, __LINE__, __FUNCTION_NAME__)
+  assert_msg_func(msg, exp, __FILE__, __LINE__, PXD_CHECKS_FUNCTION_NAME)
+#define PXD_TODO(msg)                                                          \
+  todo_func(msg, __FILE__, __LINE__, PXD_CHECKS_FUNCTION_NAME)
 #else
 #define PXD_ASSERT(exp)
 #define PXD_ASSERT_MSG(exp, msg)
