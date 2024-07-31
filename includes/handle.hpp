a7d045d55eae0c0d812c5732c2268ff2db3d849b
alpertunga-bile <kingdom19online@hotmail.com>
2024-07-31 15:35:33 +0300

    handle is updated with clang-tidy

--- includes/handle.hpp
+++ includes/handle.hpp
@@ -3,41 +3,38 @@
 #include "checks.hpp"
 
 #include <cstddef>
+#include <utility>
 
 namespace pxd {
-template<typename T>
-class Handle
-{
+template <typename T> class Handle {
 public:
   Handle() { ptr = new T(); };
-  Handle(T& value)
-  {
+  explicit Handle(T &value) {
     ptr = new T();
     *ptr = value;
   };
-  Handle(T&& value)
-  {
+  explicit Handle(T value) {
     ptr = new T();
-    *ptr = value;
+    *ptr = std::move(value);
   };
-  Handle(const Handle<T>& other) = delete;
-  Handle(Handle<T>&& other) = delete;
-  Handle& operator=(const Handle<T>& other) = delete;
+  Handle(const Handle<T> &other) = delete;
+  Handle(Handle<T> &&other) = delete;
+
+  auto operator=(const Handle<T> &other) -> Handle & = delete;
+  auto operator=(Handle<T> &&other) -> Handle & = delete;
   ~Handle() { release(); };
 
-  T& operator->() { return *ptr; }
+  auto operator->() -> T & { return *ptr; }
 
-  void recreate(T& value)
-  {
+  void recreate(T &value) {
     release();
     ptr = new T();
     *ptr = value;
   }
 
-  void recreate(T&& value) { recreate(value); }
+  void recreate(T value) { recreate(value); }
 
-  void release()
-  {
+  void release() {
     if (ptr == nullptr) {
       return;
     }
@@ -45,45 +42,41 @@ public:
     ptr = nullptr;
   }
 
-  inline T* get() { return ptr; }
-  inline T get_value() const { return *ptr; }
+  auto get() -> T * { return ptr; }
+  auto get_value() const -> T { return *ptr; }
 
 private:
-  T* ptr = nullptr;
+  T *ptr = nullptr;
 };
 
-template<typename T>
-class Array;
-template<typename T>
-class LinkedList;
+template <typename T> class Array;
+template <typename T> class LinkedList;
 
-template<typename T>
-class HandleArray
-{
+template <typename T> class HandleArray {
 public:
   HandleArray() = delete;
-  HandleArray(int size) { ptr = new T[size]; }
-  HandleArray(T* array, int size) { from_array(array, size); };
-  HandleArray(Array<T>& array)
-  {
+  explicit HandleArray(int size) { ptr = new T[size]; }
+  HandleArray(T *array, int size) { from_array(array, size); };
+  explicit HandleArray(Array<T> &array) {
     from_array(array.get_ptr(), array.get_length());
   }
-  HandleArray(LinkedList<T>& linked_list)
-  {
-    T* temp_array = new T[linked_list.get_length()];
+  explicit HandleArray(LinkedList<T> &linked_list) {
+    T *temp_array = new T[linked_list.get_length()];
     linked_list.to_array(temp_array);
 
     from_array(temp_array, linked_list.get_length());
 
     delete[] temp_array;
   }
-  HandleArray(const HandleArray<T>& other) = delete;
-  HandleArray(HandleArray<T>&& other) = delete;
-  HandleArray& operator=(const HandleArray<T>& other) = delete;
+  HandleArray(const HandleArray<T> &other) = delete;
+  HandleArray(HandleArray<T> &&other) = delete;
+
+  auto operator=(const HandleArray<T> &other) -> HandleArray & = delete;
+  auto operator=(HandleArray<T> &&other) -> HandleArray & = delete;
+
   ~HandleArray() { release(); };
 
-  decltype(auto) operator[](int index)
-  {
+  auto operator[](int index) -> decltype(auto) {
     PXD_ASSERT(index < length);
 
     if (index < 0) {
@@ -94,8 +87,7 @@ public:
     return ptr[index];
   }
 
-  void release()
-  {
+  void release() {
     if (ptr == nullptr) {
       return;
     }
@@ -105,8 +97,7 @@ public:
     length = 0;
   }
 
-  void from_array(T* array, int size)
-  {
+  void from_array(T *array, int size) {
     release();
 
     ptr = new T[size];
@@ -117,19 +108,18 @@ public:
     byte_size = size * sizeof(T);
   }
 
-  void to_array(Array<T>& array) { array.reallocate(ptr, length); }
+  void to_array(Array<T> &array) { array.reallocate(ptr, length); }
 
-  void to_linked_list(LinkedList<T>& linked_list)
-  {
+  void to_linked_list(LinkedList<T> &linked_list) {
     linked_list.from_array(ptr, length);
   }
 
-  inline T* get_array() const { return ptr; }
-  inline int get_length() const { return length; }
-  inline int get_byte_size() const { return byte_size; }
+  auto get_array() const -> T * { return ptr; }
+  auto get_length() const -> int { return length; }
+  auto get_byte_size() const -> int { return byte_size; }
 
 private:
-  T* ptr;
+  T *ptr;
   int length;
   std::size_t byte_size = 0;
 };
