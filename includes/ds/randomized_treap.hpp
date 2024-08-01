5aac1cc25e5f99eb0355248f38ae4874c0edef19
alpertunga-bile <kingdom19online@hotmail.com>
2024-08-01 11:57:20 +0300

    randomized_treap is updated with clang-tidy

--- includes/ds/randomized_treap.hpp
+++ includes/ds/randomized_treap.hpp
@@ -12,19 +12,22 @@ template <typename T> class RandomizedTreap {
 public:
   RandomizedTreap() = default;
   RandomizedTreap(const RandomizedTreap<T> &other) = default;
-  RandomizedTreap &operator=(const RandomizedTreap<T> &other) = default;
+  auto
+  operator=(const RandomizedTreap<T> &other) -> RandomizedTreap & = default;
   RandomizedTreap(RandomizedTreap &&other) = default;
-  RandomizedTreap &operator=(RandomizedTreap &&other) = default;
+  auto operator=(RandomizedTreap &&other) -> RandomizedTreap & = default;
   ~RandomizedTreap() = default;
 
-  void insert(T &key) { treap.insert(key, get_random_value(0.0, 1.0)); }
+  void insert(const T &key) {
+    treap.insert(key, pxd::random::random_value<double>(0.0, 1.0));
+  }
 
   void constains(const T &key) { return treap.contains(key); }
 
   void remove(const T &key) { return treap.remove(key); }
 
-  T min() { return treap.min(); }
-  T max() { return treap.max(); }
+  auto min() -> T { return treap.min(); }
+  auto max() -> T { return treap.max(); }
 
 private:
   Treap<T> treap;
