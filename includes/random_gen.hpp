c108fb880974bbb147b233615b48971b572f575d
alpertunga-bile <kingdom19online@hotmail.com>
2024-08-01 11:57:05 +0300

    randomize_gen is updated with abls::random and clang-tidy

--- includes/random_gen.hpp
+++ includes/random_gen.hpp
@@ -1,5 +1,59 @@
 #pragma once
 
+#include "absl/random.hpp"
+#include <cstdint>
+#include <limits>
+#include <type_traits>
+
 namespace pxd {
-double get_random_value(double min, double max);
+
+namespace random {
+
+absl::BitGen bitgen;
+
+enum class RandomClosureTypes : uint8_t {
+  CLOSED,
+  OPEN,
+  OPEN_CLOSED,
+  OPEN_OPEN,
+  CLOSED_OPEN,
+  CLOSED_CLOSED
+};
+
+template <typename T = double>
+auto random_value(
+    T min, T max,
+    RandomClosureTypes closure_type = RandomClosureTypes::CLOSED_OPEN) -> T {
+  if (!std::numeric_limits<T>::is_integer() &&
+      !std::is_floating_point<T>::value()) {
+    return {};
+  }
+
+  switch (closure_type) {
+  case RandomClosureTypes::CLOSED:
+    return absl::Uniform<T>(absl::IntervalClosed, bitgen, min, max);
+    break;
+  case RandomClosureTypes::OPEN:
+    return absl::Uniform<T>(absl::IntervalOpen, bitgen, min, max);
+    break;
+  case RandomClosureTypes::OPEN_CLOSED:
+    return absl::Uniform<T>(absl::IntervalOpenClosed, bitgen, min, max);
+    break;
+  case RandomClosureTypes::OPEN_OPEN:
+    return absl::Uniform<T>(absl::IntervalOpenOpen, bitgen, min, max);
+    break;
+  case RandomClosureTypes::CLOSED_OPEN:
+    return absl::Uniform<T>(absl::IntervalClosedOpen, bitgen, min, max);
+    break;
+  case RandomClosureTypes::CLOSED_CLOSED:
+    return absl::Uniform<T>(absl::IntervalClosedClosed, bitgen, min, max);
+    break;
+  default:
+    return {};
+    break;
+  }
 }
+
+} // namespace random
+
+} // namespace pxd
--- sources/random_gen.cpp
+++ sources/random_gen.cpp
@@ -1,14 +1,3 @@
 #include "random_gen.hpp"
 
-#include <random>
-
-std::random_device rd;
-std::mt19937 generator(rd());
-
-namespace pxd {
-double get_random_value(double min, double max) {
-  std::uniform_real_distribution<> dist(min, max);
-
-  return dist(generator);
-}
-} // namespace pxd
+namespace pxd {} // namespace pxd
